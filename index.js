d7cf6240aa8bf4cf9b5cacd2ad12bedc97ab08d5
Bernhard <Bernhard.Six@gmail.com>
2025-06-27 20:34:58 +0200

    Füge Pause dazu, damit Ausgabe sichtbar bleibt

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("Hallo Welt von MEINEM GitHub!");
+require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+}).question('Drücke Enter, um fortzufahren...', () => process.exit());
