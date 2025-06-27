6e6df9988565554e01b6bb8b74ee576bb198cc69
Bernhard <Bernhard.Six@gmail.com>
2025-06-27 22:38:04 +0200

    Füge Parameter --name hinzu

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-console.log("Hallo Welt von MEINEM GitHub!");
-require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-}).question('Drücke Enter, um fortzufahren...', () => process.exit());
+import { createInterface } from 'readline';
+
+const args = process.argv.slice(2);
+const nameFlagIndex = args.indexOf('--name');
+
+let name;
+
+if (nameFlagIndex !== -1 && args[nameFlagIndex + 1]) {
+  name = args[nameFlagIndex + 1];
+}
+
+if (!name) {
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  name = await new Promise((resolve) => {
+    readline.question('Was ist dein Name? ', (answer) => {
+      resolve(answer);
+      readline.close();
+    });
+  });
+}
+
+console.log(`Hallo ${name}!`);
